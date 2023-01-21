8f2f6ad0a0b6ee5c151a3bec0bbd0b7c12f82c5d
cmiller25 <calebmiller5354@gmail.com>
2023-01-20 19:40:12 -0600

    In process of using the minimal API into a microservice that can process orders at a coffee shop

---
+++ UserDB/DBO/CoffeeSchema/CoffeeSchema.sql
@@ -0,0 +1,49 @@
+﻿CREATE SCHEMA [Coffee];
+GO
+CREATE TABLE ORDERS(
+	[OrderID] int NOT NULL PRIMARY KEY IDENTITY,
+	[OrderStatus] int,
+	[DrinkID] int NOT NULL,
+	[Price] int NOT NULL,
+	[EmployeeID] int NOT NULL,
+	FOREIGN KEY ([OrderStatus]) REFERENCES OrderStatus([StatusID]),
+	FOREIGN KEY ([DrinkID]) REFERENCES Drinks([DrinkID]),
+	FOREIGN KEY ([EmployeeID]) REFERENCES Employees([EmployeeID])
+);
+GO
+
+CREATE TABLE Drinks(
+	[DrinkID] int NOT NULL PRIMARY KEY IDENTITY,
+	[Desc] NVARCHAR(50),
+	[Price] int NOT NULL,
+);
+GO
+
+CREATE TABLE Customers(
+	[CustomerID] int NOT NULL PRIMARY KEY IDENTITY,
+	[Fname] NVARCHAR(50),
+	[Lname] NVARCHAR(50),
+	[RewardsMember] BIT NOT NULL
+);
+GO
+
+CREATE TABLE Employees(
+	[EmployeeID] int NOT NULL PRIMARY KEY IDENTITY,
+	[Fname] NVARCHAR(50) NOT NULL,
+	[Lname] NVARCHAR(50) NOT NULL,
+	[dateOfHire] int NOT NULL
+);
+GO
+
+CREATE TABLE OrderStatus(
+	[StatusID] int NOT NULL PRIMARY KEY IDENTITY,
+	[Desc] NVARCHAR(50) NOT NULL
+);
+GO
+
+CREATE TABLE Rewards(
+	[CustomerID] int NOT NULL,
+	[OrderAmounts] int,
+	[TotalPriceSpent] int,
+	[DateAccCreated] int
+);
--- UserDB/Script.PostDeployment.sql
+++ UserDB/Script.PostDeployment.sql
@@ -5,5 +5,39 @@ begin
 	('Sue', 'Storm'),
 	('Tim', 'Timmerson'),
 	('Mary', 'Miller');
-end
+end;
+
+if not exists (select 1 from dbo.[CoffeeSchema])
+begin
+	
+	insert into dbo.[Orders] (OrderStatus, Price)
+	values ('Unfinished', 50),
+	('Ready', 20),
+	('In progress', 10);
+	
+	insert into dbo.Drinks([Desc], Price)
+	values ('Latte', 5),
+	('Medium roast', 3),
+	('Macchiato', 6);
+
+	insert into dbo.[Customers] (Fname, Lname, RewardsMember)
+	values ('Jon', 'Johnson', 1),
+	('Susan', 'Smitherson', 0),
+	('Tammy', 'Tammerson', 1);
+
+	insert into dbo.[Employees] (Fname, Lname, dateOfHire)
+	values ('Thor', 'Odinson', 1999),
+	('Jimmy', 'John', 2005),
+	('Sue', 'Storm', 2003);
+
+	insert into dbo.[OrderStatus] ([Desc])
+	values ('In progress'),
+	('Ready'),
+	('Just started');
+
+	insert into dbo.[Rewards] (OrderAmounts, TotalPriceSpent, DateAccCreated)
+	values (2000, 500, 1999),
+	(3000, 200, 2005),
+	(400, 100, 2015);
+end;
 -- Default data if no input exists
